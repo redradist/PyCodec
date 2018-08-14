a7af303b9efef54169c5f187fc68c7a379f97ec7
redradist <redradist@gmail.com>
2018-08-14 22:11:52 +0300

    Added tests for SHA224

--- pycodec/crypto/valid/sha2.py
+++ pycodec/crypto/valid/sha2.py
@@ -18,6 +18,127 @@ class SHA2:
                  0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2]
 
     @staticmethod
+    def hash_sha224(msg):
+        h0 = BitArray(uint=0xC1059ED8, length=32)
+        h1 = BitArray(uint=0x367cd507, length=32)
+        h2 = BitArray(uint=0x3070dd17, length=32)
+        h3 = BitArray(uint=0xf70e5939, length=32)
+        h4 = BitArray(uint=0xffc00b31, length=32)
+        h5 = BitArray(uint=0x68581511, length=32)
+        h6 = BitArray(uint=0x64f98fa7, length=32)
+        h7 = BitArray(uint=0xbefa4fa4, length=32)
+
+        bit_msg = BitArray(msg)
+        len_msg = len(bit_msg)
+        bit_msg += BitArray(bin='1')
+        k = (448 + 512 - (len_msg % 512 + 1)) % 512
+        bit_msg += BitArray(bin='0' * k)
+        bit_msg += BitArray(int=len_msg, length=64)
+        num_chunks = len(bit_msg) // 512
+        assert len(bit_msg) % 512 == 0
+
+        bit_msg_chunks = list(bit_msg[512 * num_chunk:
+                                      512 * (num_chunk + 1)]
+                              for num_chunk in range(num_chunks))
+        for bit_msg_chunk in bit_msg_chunks:
+            msg_sub_chunks = list(bit_msg_chunk[32 * num_chunk:
+                                                32 * (num_chunk + 1)]
+                                  for num_chunk in range(16))
+            for i in range(16, 64):
+                chunk_i_15 = msg_sub_chunks[i - 15]
+                temp0 = copy.deepcopy(chunk_i_15)
+                temp0.ror(7)
+                temp1 = copy.deepcopy(chunk_i_15)
+                temp1.ror(18)
+                temp2 = copy.deepcopy(chunk_i_15)
+                temp2 = temp2 >> 3
+                s0 = temp0 ^ temp1 ^ temp2
+
+                chunk_i_2 = msg_sub_chunks[i - 2]
+                temp0 = copy.deepcopy(chunk_i_2)
+                temp0.ror(17)
+                temp1 = copy.deepcopy(chunk_i_2)
+                temp1.ror(19)
+                temp2 = copy.deepcopy(chunk_i_2)
+                temp2 = temp2 >> 10
+                s1 = temp0 ^ temp1 ^ temp2
+
+                msg_sub_chunks.append(BitArray(
+                    uint=(msg_sub_chunks[i - 16].uint + s0.uint + msg_sub_chunks[i - 7].uint + s1.uint) % (2 ** 32),
+                    length=32))
+
+            a = copy.deepcopy(h0)
+            b = copy.deepcopy(h1)
+            c = copy.deepcopy(h2)
+            d = copy.deepcopy(h3)
+            e = copy.deepcopy(h4)
+            f = copy.deepcopy(h5)
+            g = copy.deepcopy(h6)
+            h = copy.deepcopy(h7)
+
+            for i in range(0, 64):
+                temp0 = copy.deepcopy(a)
+                temp0.ror(2)
+                temp1 = copy.deepcopy(a)
+                temp1.ror(13)
+                temp2 = copy.deepcopy(a)
+                temp2.ror(22)
+                Sum0 = temp0 ^ temp1 ^ temp2
+
+                Ma = (a & b) ^ (a & c) ^ (b & c)
+
+                t2 = BitArray(uint=(Sum0.uint + Ma.uint) % (2 ** 32),
+                              length=32)
+
+                temp0 = copy.deepcopy(e)
+                temp0.ror(6)
+                temp1 = copy.deepcopy(e)
+                temp1.ror(11)
+                temp2 = copy.deepcopy(e)
+                temp2.ror(25)
+                Sum1 = temp0 ^ temp1 ^ temp2
+
+                temp0 = e & f
+                temp1 = ~e
+                temp2 = copy.deepcopy(g)
+                Ch = temp0 ^ (temp1 & temp2)
+
+                t1 = BitArray(
+                    uint=(h.uint + Sum1.uint + Ch.uint + SHA2.constants[i] + msg_sub_chunks[i].uint) % (2 ** 32),
+                    length=32)
+
+                h = copy.deepcopy(g)
+                g = copy.deepcopy(f)
+                f = copy.deepcopy(e)
+                e = BitArray(uint=(d.uint + t1.uint) % (2 ** 32),
+                             length=32)
+                d = copy.deepcopy(c)
+                c = copy.deepcopy(b)
+                b = copy.deepcopy(a)
+                a = BitArray(uint=(t1.uint + t2.uint) % (2 ** 32),
+                             length=32)
+
+            h0 = BitArray(uint=(h0.uint + a.uint) % (2 ** 32),
+                          length=32)
+            h1 = BitArray(uint=(h1.uint + b.uint) % (2 ** 32),
+                          length=32)
+            h2 = BitArray(uint=(h2.uint + c.uint) % (2 ** 32),
+                          length=32)
+            h3 = BitArray(uint=(h3.uint + d.uint) % (2 ** 32),
+                          length=32)
+            h4 = BitArray(uint=(h4.uint + e.uint) % (2 ** 32),
+                          length=32)
+            h5 = BitArray(uint=(h5.uint + f.uint) % (2 ** 32),
+                          length=32)
+            h6 = BitArray(uint=(h6.uint + g.uint) % (2 ** 32),
+                          length=32)
+            h7 = BitArray(uint=(h7.uint + h.uint) % (2 ** 32),
+                          length=32)
+
+        hash = h0 + h1 + h2 + h3 + h4 + h5 + h6
+        return hash.tobytes()
+
+    @staticmethod
     def hash_sha256(msg):
         h0 = BitArray(uint=0x6a09e667, length=32)
         h1 = BitArray(uint=0xbb67ae85, length=32)
--- tests/crypto/valid/sha2_tests.py
+++ tests/crypto/valid/sha2_tests.py
@@ -3,7 +3,7 @@ import unittest
 from pycodec.crypto.valid.sha2 import SHA2
 
 
-class Testing_SHA_256(unittest.TestCase):
+class Testing_SHA2(unittest.TestCase):
     def setUp(self):
         """Currently nothing to do. Use it for initialization data before test"""
         pass
@@ -12,6 +12,10 @@ class Testing_SHA_256(unittest.TestCase):
         """Currently nothing to do. Use it for reinitialization data after test"""
         pass
 
-    def test__SHA_256_Encrypt_PeriodicA__Valid(self):
+    def test__SHA224_Encrypt_PeriodicA__Valid(self):
+        sha_224 = SHA2.hash_sha224(bytes('The quick brown fox jumps over the lazy cog', encoding='ascii'))
+        self.assertEqual(sha_224, bytes.fromhex('fee755f44a55f20fb3362cdc3c493615b3cb574ed95ce610ee5b1e9b'))
+
+    def test__SHA256_Encrypt_PeriodicA__Valid(self):
         sha_256 = SHA2.hash_sha256(bytes('The quick brown fox jumps over the lazy cog', encoding='ascii'))
         self.assertEqual(sha_256, bytes.fromhex('e4c4d8f3bf76b692de791a173e05321150f7a345b46484fe427f6acc7ecc81be'))
