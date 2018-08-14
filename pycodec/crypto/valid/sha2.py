bbf279ea290c172365fcae32ccfdb2725d7424f8
redradist <redradist@gmail.com>
2018-08-14 22:04:08 +0300

    Renamed discription of SHA2 class

--- pycodec/crypto/valid/sha2.py
+++ pycodec/crypto/valid/sha2.py
@@ -6,7 +6,7 @@ from bitstring import BitArray
 
 class SHA2:
     """
-    SHA_256 hash function
+    Family of SHA2 hash functions
     """
     constants = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
                  0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
